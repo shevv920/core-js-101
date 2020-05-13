cc6cf8a1e3932a894655875f5b4b2285d7f3761d
Vladimir Shevtsov <shevvv@tutanota.com>
2020-05-13 23:47:59 +0300

    feat: add polynom solution

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -62,8 +62,8 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (x) => [...args].reverse().reduce((acc, cur, i) => acc + cur * (x ** i));
 }
 
 
